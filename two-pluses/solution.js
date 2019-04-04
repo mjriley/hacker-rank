175698dfaf86964fd4dce8997bb997e6f786ca3e
Matthew Riley <mjriley2@gmail.com>
2019-04-03 23:41:22 -0700

    Progress on two pluses

---
+++ two-pluses/solution.js
@@ -0,0 +1,126 @@
+'use strict';
+/*
+Observations:
+The grid is, at worst, 15x15, which means, to me, that brute-forcing the problem by finding 
+the largest plus from any given square is a reasonable approach. However, finding the next largest,
+non-overlapping plus for every square would then be an order of magnitude worse for the search space, 
+which is not ideal.
+Ideally, I would like to find the largest plus, label those squares as bad, and then find the next largest plus, but the issue is that
+it is possible to have the largest plus its next largest non-overlapping plus be smaller than if we had found a smaller initial plus
+(i.e. we might get a 5 plus with a 1 plus = 9 rather than using 2 3-pluses = 25)
+
+One situation to keep in mind is the scenario where two 'largest' pluses exist, say 2 p5s, but they'll overlap each other only when p5s.
+A p5 is trivially also a p3, and that p3 may not overlap
+
+
+We will need functions:
+- getLargestPlusAtSquare()
+- createNonOverlappedGrid(x, y, plusSize, grid)
+- doesPlusOverlap(x, y, plusSize, nonOverlappedGrid)
+*/
+
+/* New approach:
+- Find the largest plus for each square. Create a hashmap, and store a list for each size of plus
+- Iterate through the largest plus size list
+- For each of these pluses, check any other plus in the list doesn't overlap. If so, this becomes our pair of pluses
+- If not, check each of these pluses against the list of pluses with n-1 size. Remember to include the pluses with n size in that list too
+- That becomes the largest product for a plus with N size.
+- Now repeat the process for pluses with N-1 size. If we ever find a pair with a greater product, then return it, because that is the largest we'll find
+- Otherwise, when we start checking against plues with a certain size (equal to the second plus in the initial search), return our original value.
+
+function twoPluses(grid) {
+    return 0;
+}
+*/
+
+function twoPluses() {}
+
+const BAD = 'B';
+const GOOD = 'G';
+
+function getLargestPlus(x, y, grid) {
+    // expand out-ward from the center, checking each neighbor until we find a bad neighbor
+    let currentSize = 1;
+    let radius = 1;
+
+    const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+    while (true) {
+        for (let i = 0; i < directions.length; i++) {
+            const direction = directions[i];
+            const [newX, newY] = [
+                x + direction[0] * radius,
+                y + direction[1] * radius
+            ];
+
+            // actually don't need to check the edges of the grid, as accessing those squares
+            // will return undefined
+            if (grid[newX][newY] !== GOOD) {
+                return currentSize;
+            }
+        }
+        currentSize += 2;
+        radius++;
+    }
+
+    return 1;
+}
+
+function getArea(plusSize) {
+    return plusSize * 2 - 1;
+}
+
+function doesOverlap(plusLeft, plusRight) {
+    // plus format is:
+    // { size, x, y}
+
+    if (plusLeft.x === plusRight.x && plusLeft.y === plusRight.y) {
+        return true;
+    }
+
+    const leftEdgeSize = Math.floor(plusLeft.size / 2);
+    const rightEdgeSize = Math.floor(plusRight.size / 2);
+
+    const verticalDistanceBetweenCenters = Math.abs(plusLeft.y - plusRight.y);
+    const horizontalDistanceBetweenCenters = Math.abs(plusLeft.x - plusRight.x);
+
+    const horizontallyOverlaps =
+        horizontalDistanceBetweenCenters < leftEdgeSize + rightEdgeSize;
+    const verticallyOverlaps =
+        verticalDistanceBetweenCenters < leftEdgeSize + rightEdgeSize;
+
+    console.log(`overlaps: ${horizontallyOverlaps} ${verticallyOverlaps}`);
+
+    if (plusLeft.y === plusRight.y) {
+        return horizontallyOverlaps;
+    }
+
+    if (plusLeft.x === plusRight.x) {
+        return verticallyOverlaps;
+    }
+
+    return horizontallyOverlaps && verticallyOverlaps;
+    // if (
+    //     verticalDistanceBetweenCenters > leftEdgeSize + rightEdgeSize ||
+    //     horizontalDistanceBetweenCenters > leftEdgeSize + rightEdgeSize
+    // ) {
+    //     return false; // no collision is possible.
+    // }
+
+    // if (plusLeft.y === plusRight.y) {
+    //     return leftEdgeSize + rightEdgeSize >= horizontalDistanceBetweenCenters;
+    // }
+
+    // if (plusLeft.x === plusRight.x) {
+    //     const differenceBetweenCenters = Math.abs(plusLeft.y - plusRight.y);
+
+    //     return leftEdgeSize + rightEdgeSize >= verticalDistanceBetweenCenters;
+    // }
+}
+
+module.exports = {
+    twoPluses,
+    getLargestPlus,
+    getArea,
+    doesOverlap
+};
---
+++ two-pluses/solution.test.js
@@ -0,0 +1,90 @@
+const { getLargestPlus, getArea, doesOverlap } = require('./solution');
+
+describe('getLargestPlus', () => {
+    test('it returns 1 when the cell is surrounded by bad cells', () => {
+        const input = ['BBB', 'BGB', 'BBB'];
+
+        expect(getLargestPlus(1, 1, input)).toBe(1);
+    });
+
+    test('it expands on all sides to the first bad block', () => {
+        const input = ['BBBBB', 'BBGBB', 'GGGGG', 'BBGBB', 'BBGBB'];
+        expect(getLargestPlus(2, 2, input)).toBe(3);
+    });
+
+    test('it recognizes the edges as invalid', () => {
+        const input = ['BBGB', 'BBGB', 'GGGG', 'BBGB', 'BBGB'];
+
+        expect(getLargestPlus(2, 2, input)).toBe(3);
+    });
+
+    test('it handles when the edges are even', () => {
+        const input = ['BBGBB', 'BBGBB', 'GGGGG', 'BBGBB', 'BBGBB'];
+
+        expect(getLargestPlus(2, 2, input)).toBe(5);
+    });
+});
+
+describe('getArea', () => {
+    test('handles a single square', () => {
+        expect(getArea(1)).toBe(1);
+    });
+
+    test('handles multi-cell pluses', () => {
+        expect(getArea(5)).toBe(9);
+    });
+});
+
+describe('doesOverlap', () => {
+    test('it handles no overlaps', () => {
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 2 }, { size: 3, x: 5, y: 2 })
+        ).toBe(false);
+
+        expect(
+            doesOverlap({ size: 3, x: 5, y: 2 }, { size: 3, x: 2, y: 2 })
+        ).toBe(false);
+    });
+
+    test('detects overlaps when they share a center', () => {
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 2 }, { size: 1, x: 2, y: 2 })
+        ).toBe(true);
+    });
+
+    test('detects when they overlap horizontally', () => {
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 2 }, { size: 3, x: 4, y: 2 })
+        ).toBe(true);
+        expect(
+            doesOverlap({ size: 3, x: 4, y: 2 }, { size: 3, x: 2, y: 2 })
+        ).toBe(true);
+    });
+
+    test('detects when they do not overlap vertically', () => {
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 2 }, { size: 3, x: 2, y: 5 })
+        ).toBe(false);
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 5 }, { size: 3, x: 2, y: 2 })
+        ).toBe(false);
+    });
+
+    test('detects when they overlap vertically', () => {
+        expect(
+            doesOverlap({ size: 3, x: 2, y: 2 }, { size: 3, x: 2, y: 4 })
+        ).toBe(true);
+    });
+
+    test('detects vertical and horizontal overlap', () => {
+        expect(
+            doesOverlap({ size: 5, x: 3, y: 3 }, { size: 5, x: 1, y: 2 })
+        ).toBe(true);
+    });
+
+    test.only('handles when pluses touch', () => {
+        expect(
+            doesOverlap({ size: 5, x: 3, y: 3 }, { size: 3, x: 1, y: 2 })
+        ).toBe(false);
+    });
+});
